23ce9ea4c30ad265b2a8bb92b0c0351c7477c66f
anshsaxena-gemini <ansh.saxena@geminisolutions.com>
2022-06-22 17:05:14 +0530

    added

--- src/components/Assignment_01/AddDetails.js
+++ src/components/Assignment_01/AddDetails.js
@@ -1,33 +1,41 @@
 import React,{useState} from 'react'
-import './AddDetails.css';
+import styles from './AddDetails.module.css';
 
 function AddDetails() {
     const [input,setInput] = useState("");
     const [list,setList] = useState([]);
-
+    const [error,setError] = useState(false);
     
-   
-
+  
     const handleSubmit = () =>{
-    
+       
         const data = {input}
+        // 
+        if(input.trim().length === 0){
+          setError(true);
+        }
+        else{
+          setError(false)
         setList((ls) =>
             [
              ...ls,data
             
             ]
         )
-        setInput("")
+          }
+       
     }
    
   return (
 
-    <div className='Container'>
-  <input type='text' 
+    <div className={styles.Container}>
+  <input 
+  className={`${styles.userInput} ${error ? styles.invalid : ''}`}
+  type='text' 
   placeholder='type something' 
   onChange={(e)=>setInput(e.target.value)}
   ></input>
-   <button className='btn' type='button'  onClick={handleSubmit}>Add</button><br></br>
+   <button className={styles.btn} type='button' onClick={handleSubmit}>Add</button><br></br>
    {
      list.map((data)=>(
        
